d0fca7c7f58a41c398decb7e98404fcf25d4ab79
Sai Satya Veer Madhavarapu <106660282+21MH5A0337@users.noreply.github.com>
2022-07-18 17:25:50 +0530

    def prm(n):  fc=0  for i in range(1,n+1):    if n%i==0:       fc+=1  if fc==2:     return True  else:     return False
    n=int(input())
    for i in range(1,n+1):
      if n%i==0:
         if i!=n//i and prm(i)==True and prm(n//i)==True:
            print(i,n//i)
            break
    else:
      print(-1)

---
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -0,0 +1,18 @@
+def prm(n):
+ fc=0
+ for i in range(1,n+1):
+   if n%i==0:
+      fc+=1
+ if fc==2:
+    return True
+ else:
+    return False
+        
+n=int(input())
+for i in range(1,n+1):
+  if n%i==0:
+     if i!=n//i and prm(i)==True and prm(n//i)==True:
+        print(i,n//i)
+        break
+else:
+  print(-1)
